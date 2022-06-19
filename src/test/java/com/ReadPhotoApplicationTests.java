901e8d5a26654821099be4341a06203ba6a29c1d
jose de los santos <delossantosramosjose@gmail.com>
2022-06-19 20:38:44 +0200

    created repository

---
+++ src/test/java/com/ReadPhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadPhotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
